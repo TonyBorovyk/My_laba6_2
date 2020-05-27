9970c1beb6327fb8b6dcd16bdef40a8d7f430940
Anton Boovyk <antonbovik@gmail.com>
2020-05-27 11:47:46 +0300

    add

--- src/com/company/ArrayForSort.java
+++ src/com/company/ArrayForSort.java
@@ -7,29 +7,29 @@ import java.util.function.Predicate;
 
 public class ArrayForSort<T> {
 
-    final T[] mainArray;
+    final T[] Mainarr;
     int numberOfElements;
 
     ArrayForSort(Class<T> template, int numberOfElements) throws InstantiationException, IllegalAccessException {
         this.numberOfElements = numberOfElements;
-        mainArray = (T[]) Array.newInstance(template, numberOfElements);
+        Mainarr = (T[]) Array.newInstance(template, numberOfElements);
         fillArray(template);
     }
 
     void fillArray(Class<T> template) throws IllegalAccessException, InstantiationException {
-        for (int i = 0; i < mainArray.length; i++) {
-            mainArray[i] = template.newInstance();
+        for (int i = 0; i <Mainarr.length; i++) {
+            Mainarr[i] = template.newInstance();
         }
     }
     void output(){
-        for (T t : mainArray) {
+        for (T t :Mainarr) {
             System.out.println(t.toString());
         }
     }
 
     ArrayList<T> getPart(Predicate<T> predicate){
         ArrayList<T> list = new ArrayList<>();
-        for(T temp: mainArray){
+        for(T temp: Mainarr){
             if(predicate.test(temp))
                 list.add(temp);
         }
